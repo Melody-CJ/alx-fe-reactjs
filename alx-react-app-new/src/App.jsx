10624dad653f5a8026dd2210e8324121bfdca2c6
Melody Arinze <arinze.melody2999@gmail.com>
2025-07-19 22:09:25 +0100

    Add Counter component with useState hook

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -16,14 +17,15 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />  
-      <Footer />
       <UserProfile 
-      name="Alice" 
-      age="25" 
-      bio="Loves hiking and photography" 
+      name="Melody" 
+      age={25} 
+      bio="Aspiring frontend developer." 
       />
+      <Counter />
+      <Footer />
 
-      <div>
+      <div> 
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,6 +1,6 @@
 function MainContent() {
     return (
-        <main style={{ backgroundColor: '#f9f9f9', padding: '20px', minHeight: '200px' }}>
+        <main style={{ backgroundColor: '#f9f9f9', color: '#000000', padding: '20px', minHeight: '200px' }}>
              <p>I love to visit New York, Paris, and Tokyo.</p>
         </main>
     );
