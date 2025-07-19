b5d7f65c2e068ccd1c87fffd06a9ad6a0120f482
Melody Arinze <arinze.melody2999@gmail.com>
2025-07-19 21:56:01 +0100

    Add new version of React app with inline styling

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main style={{ backgroundColor: '#f9f9f9', padding: '20px', minHeight: '200px' }}>
+             <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
