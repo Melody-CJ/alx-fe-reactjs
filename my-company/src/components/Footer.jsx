be318fb95a0324456c5b958e5e44d1cbd7b7a6e5
Melody Arinze <arinze.melody2999@gmail.com>
2025-07-20 19:35:52 +0100

    Add a Simple Company Website with React

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+      <footer style={{ backgroundColor: '#333', color: '#fff', padding: '10px', textAlign: 'center' }}>
+        <p>© 2025 Our Company. All rights reserved.</p>
+      </footer>
+    );
+  }
+  export default Footer;
+  
