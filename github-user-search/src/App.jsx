3cac6572fd88294f95eb033b3a1837d4de2e2d42
Melody Arinze <arinze.melody2999@gmail.com>
2025-08-03 15:37:36 +0100

    Set up a react applicationn for a github user application

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+
+const App  = () => {
+  return (
+    <div>
+      <h1>Github User Search</h1>
+      {/* Components like <SearchBar /> and <UserCard /> will go here*/}
+    </div>
+  );
+};
+
+export default App
---
+++ github-user-search/src/services/githubApi.js
@@ -0,0 +1,1 @@
+const BASE_URL = import.meta.env.VITE_GITHUB_API_URL;
